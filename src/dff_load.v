fbaf4462dd0c80e7b272ae5e257cfa8f5b9f2332
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-12 14:12:18 +0530

    Add D flip-flop with load enable design file

---
+++ src/dff_load.v
@@ -0,0 +1,15 @@
+module dff_load(
+    input wire clk,
+    input wire reset,
+    input wire load,
+    input wire d,
+    output reg q
+);
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset) q<=1'b0;
+    else if(load) q<=d;
+    else q<=q;
+end
+endmodule
