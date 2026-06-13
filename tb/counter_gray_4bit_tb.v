7732bb9e1d6a6c115b7045f83f2a0465ac79f154
B. Abhi Chandra <24l31a0436@vignaniit.edu.in>
2026-06-13 20:51:01 +0530

    Add Gray code counter testbench

---
+++ tb/counter_gray_4bit_tb.v
@@ -0,0 +1,39 @@
+`timescale 1ns/1ps
+
+module counter_gray_4bit_tb;
+
+reg clk;
+reg reset;
+reg enable;
+wire [3:0] count;
+
+counter_gray_4bit uut(
+    .clk(clk),
+    .reset(reset),
+    .enable(enable),
+    .count(count)
+);
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+initial begin
+
+    $dumpfile("counter_gray_4bit.vcd");
+    $dumpvars(0,counter_gray_4bit_tb);
+
+    $monitor("Time=%0t,clk=%b,reset=%b,enable=%b,count=%b",$time,clk,reset,enable,count);
+
+    reset=1; enable=0; #12;
+    reset=0; enable=1; #170;
+    enable=0; #20;
+    enable=1; #20;
+    reset=1; #5;
+    reset=0; #20;
+
+    $finish;
+
+end
+endmodule
